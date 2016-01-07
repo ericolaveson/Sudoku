b4881253653196484fe0fe9d611c0bf37617a360
ericolaveson <agentericolaveson@gmail.com>
2016-01-06 22:05:15 -0800

    Latest and Greatest

--- Sudoku.java
+++ Sudoku.java
@@ -7,7 +7,6 @@
  *    normal form (CNF) boolean statements. These will then be used as an
  *    input file for the minisat solver.
  *****************************************************************************/
-//derp
 import java.io.*;
 import java.util.ArrayList;
 
